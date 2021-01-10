bf1320ab36d74000e2bd3a692b7f5050ea87488a
Ngô Đạt <supodatngo123@gmail.com>
2021-01-10 12:44:21 +0700

    sua lai form

--- WF_Expert_System/Form1.Designer.cs
+++ WF_Expert_System/Form1.Designer.cs
@@ -29,167 +29,105 @@ namespace WF_Expert_System
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.button1_click = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.contentchoiceListbox = new System.Windows.Forms.ListBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button_clickReset = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // checkedListBox1
-            // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Items.AddRange(new object[] {
-            "Dau hong",
-            "Dau da day",
-            "Dau dau",
-            "Chong mat",
-            "Dau chan",
-            "Dau tai",
-            "Dau Hong",
-            "So Mui"});
-            this.checkedListBox1.Location = new System.Drawing.Point(14, 33);
-            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(145, 202);
-            this.checkedListBox1.TabIndex = 0;
-            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
-            // 
-            // button1_click
-            // 
-            this.button1_click.Location = new System.Drawing.Point(13, 265);
-            this.button1_click.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.button1_click.Name = "button1_click";
-            this.button1_click.Size = new System.Drawing.Size(145, 27);
-            this.button1_click.TabIndex = 1;
-            this.button1_click.Text = "Enter";
-            this.button1_click.UseVisualStyleBackColor = true;
-            this.button1_click.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(533, 309);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Location = new System.Drawing.Point(652, 460);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(89, 27);
+            this.button2.Size = new System.Drawing.Size(111, 34);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 31);
-            this.label1.TabIndex = 6;
-            // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(409, 33);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.richTextBox1.Location = new System.Drawing.Point(511, 65);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(218, 257);
+            this.richTextBox1.Size = new System.Drawing.Size(272, 320);
             this.richTextBox1.TabIndex = 4;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(13, 353);
-            this.richTextBox2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.richTextBox2.Location = new System.Drawing.Point(23, 521);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(609, 116);
+            this.richTextBox2.Size = new System.Drawing.Size(760, 144);
             this.richTextBox2.TabIndex = 5;
             this.richTextBox2.Text = "";
             this.richTextBox2.TextChanged += new System.EventHandler(this.button1_Click);
             // 
+            // checkedListBox
+            // 
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Location = new System.Drawing.Point(16, 65);
+            this.checkedListBox.Name = "checkedListBox";
+            this.checkedListBox.Size = new System.Drawing.Size(264, 340);
+            this.checkedListBox.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 25);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "How do you feel ?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(337, 65);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(112, 34);
+            this.Start.TabIndex = 15;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(337, 128);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(112, 34);
+            this.Reset.TabIndex = 16;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(453, 0);
+            this.label2.Location = new System.Drawing.Point(541, 20);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 20);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
-            // 
-            // contentchoiceListbox
-            // 
-            this.contentchoiceListbox.FormattingEnabled = true;
-            this.contentchoiceListbox.ItemHeight = 20;
-            this.contentchoiceListbox.Location = new System.Drawing.Point(190, 33);
-            this.contentchoiceListbox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.contentchoiceListbox.Name = "contentchoiceListbox";
-            this.contentchoiceListbox.Size = new System.Drawing.Size(154, 124);
-            this.contentchoiceListbox.TabIndex = 8;
-            this.contentchoiceListbox.SelectedIndexChanged += new System.EventHandler(this.contentchoiceListbox_SelectedIndexChanged);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(126, 20);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "How do you feel?";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(190, 167);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(154, 124);
-            this.listBox1.TabIndex = 10;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(190, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(257, 20);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Do you have the following symptom?";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
-            // button_clickReset
-            // 
-            this.button_clickReset.Location = new System.Drawing.Point(13, 305);
-            this.button_clickReset.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button_clickReset.Name = "button_clickReset";
-            this.button_clickReset.Size = new System.Drawing.Size(145, 31);
-            this.button_clickReset.TabIndex = 12;
-            this.button_clickReset.Text = "Reset";
-            this.button_clickReset.UseVisualStyleBackColor = true;
-            this.button_clickReset.Click += new System.EventHandler(this.button_clickReset_Click);
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Result";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 545);
-            this.Controls.Add(this.button_clickReset);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.contentchoiceListbox);
+            this.ClientSize = new System.Drawing.Size(800, 814);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1_click);
-            this.Controls.Add(this.checkedListBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -199,20 +137,15 @@ namespace WF_Expert_System
         }
 
         #endregion
-
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
-        private System.Windows.Forms.Button button1;
+        
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Reset;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox contentchoiceListbox;
-        private System.Windows.Forms.Button button1_click;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button_clickReset;
     }
 }
 
--- WF_Expert_System/Form1.cs
+++ WF_Expert_System/Form1.cs
@@ -10,77 +10,88 @@ using System.Windows.Forms;
 
 namespace WF_Expert_System
 {
+    
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            //knowledgeBase = new KnowledgeBase(@"C:\Users\supod\source\repos\WF_Expert_System\WF_Expert_System\Knowlegde.txt");
+            //inferenceEngine = new InferenceEngine(knowledgeBase);
+            //checkedListBox = new CheckedListBox();
+            //for (int i = 0; i < inferenceEngine.Base.Parameters.Count; i++)
+            //{
+            //    checkedListBox.Items.Add(inferenceEngine.Base.Parameters[i].Name, false);
+            //}
+            //Controls.Add(checkedListBox);
+            //inferenceEngine.Start();
+            //inferenceEngine.ShowResult();
         }
-       
+
         private void button1_Click(object sender, EventArgs e)
         {
-            Button button = new Button();
-            listBox1.Items.Clear();
-            foreach (string s in checkedListBox1.CheckedItems)
-            {
-                listBox1.Items.Add(s);
-            }
-            contentchoiceListbox.Items.Clear();
-            for (int i = 0; i < checkedListBox1.CheckedIndices.Count; i++)
-            {
-                contentchoiceListbox.Items.Add(checkedListBox1.CheckedIndices[i] + 1);
-            }
-            richTextBox1.Text = "You should: ";
-            //foreach (int indexChecked in checkedListBox1.CheckedIndices)
+            //Button button = new Button();
+            //listBox1.Items.Clear();
+            //foreach (string s in checkedListBox.CheckedItems)
             //{
-            //    MessageBox.Show("Index: " + indexChecked.ToString() + ", is checked. Checked state is: " +
-            //        checkedListBox1.GetItemCheckState(indexChecked).ToString() + ".");
+            //    listBox1.Items.Add(s);
             //}
-            bool test1 = false;
-            bool test6 = false;
-            for (int i = 0; i < checkedListBox1.CheckedIndices.Count; i++)
-            {
-                if(checkedListBox1.CheckedIndices[i] +1 ==1)
-                {
-                 DialogResult result1 =  MessageBox.Show("Rát họng?","Chi tiết triệu chứng", MessageBoxButtons.YesNo); 
-                 DialogResult result2 = MessageBox.Show("Thường xuyên khát nước?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                 DialogResult result3 = MessageBox.Show("Bạn có đau đầu không?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                    test1 = true; // Đã có triệu chứng 1
-                }
-                if (checkedListBox1.CheckedIndices[i] + 1 == 6)
-                {
-                    DialogResult result4 = MessageBox.Show("Sốt cao?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                    if (test1 == false) // Đã có triêu chứng 1 rồi thì ko hỏi nhức đầu hay ko nữa
-                    {
-                        DialogResult result3 = MessageBox.Show("Nhức đầu?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                    }
-                    test6 = true;
-                }
-                if (checkedListBox1.CheckedIndices[i] + 1 == 8)
-                {
-                    if (test6 == true)
-                    {
-                        DialogResult result4 = MessageBox.Show("Sốt cao?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                    }
-                    DialogResult result5 = MessageBox.Show("Ngạt mũi?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                    DialogResult result6 = MessageBox.Show("Nhức âm ĩ vùng mặt?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                    DialogResult result7 = MessageBox.Show("Ho có đờm?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //contentchoiceListbox.Items.Clear();
+            //for (int i = 0; i < checkedListBox1.CheckedIndices.Count; i++)
+            //{
+            //    contentchoiceListbox.Items.Add(checkedListBox1.CheckedIndices[i] + 1);
+            //}
+            //richTextBox1.Text = "You should: ";
+            ////foreach (int indexChecked in checkedListBox1.CheckedIndices)
+            ////{
+            ////    MessageBox.Show("Index: " + indexChecked.ToString() + ", is checked. Checked state is: " +
+            ////        checkedListBox1.GetItemCheckState(indexChecked).ToString() + ".");
+            ////}
+            //bool test1 = false;
+            //bool test6 = false;
+            //for (int i = 0; i < checkedListBox1.CheckedIndices.Count; i++)
+            //{
+            //    if(checkedListBox1.CheckedIndices[i] +1 ==1)
+            //    {
+            //     DialogResult result1 =  MessageBox.Show("Rát họng?","Chi tiết triệu chứng", MessageBoxButtons.YesNo); 
+            //     DialogResult result2 = MessageBox.Show("Thường xuyên khát nước?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //     DialogResult result3 = MessageBox.Show("Bạn có đau đầu không?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //        test1 = true; // Đã có triệu chứng 1
+            //    }
+            //    if (checkedListBox1.CheckedIndices[i] + 1 == 6)
+            //    {
+            //        DialogResult result4 = MessageBox.Show("Sốt cao?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //        if (test1 == false) // Đã có triêu chứng 1 rồi thì ko hỏi nhức đầu hay ko nữa
+            //        {
+            //            DialogResult result3 = MessageBox.Show("Nhức đầu?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //        }
+            //        test6 = true;
+            //    }
+            //    if (checkedListBox1.CheckedIndices[i] + 1 == 8)
+            //    {
+            //        //if (test6 == true)
+            //        //{
+            //        //    DialogResult result4 = MessageBox.Show("Sốt cao?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //        //}
+            //        //DialogResult result5 = MessageBox.Show("Ngạt mũi?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //        //DialogResult result6 = MessageBox.Show("Nhức âm ĩ vùng mặt?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //        //DialogResult result7 = MessageBox.Show("Ho có đờm?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
 
-                    if (test1 == false)
-                    {
-                        DialogResult result1 = MessageBox.Show("Ngạt mũi?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                    }
-                    if(test6 == false)
-                    {
-                        DialogResult result4 = MessageBox.Show("Sốt cao?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                    }
-                    DialogResult result8 = MessageBox.Show("Đau nhói vùng họng?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                    DialogResult result9 = MessageBox.Show("Khan tiếng/ Tắt tiếng?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                    DialogResult result10 = MessageBox.Show("Hôi hơi thở?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
-                }
-      
-            }
+            //        //if (test1 == false)
+            //        //{
+            //        //    DialogResult result1 = MessageBox.Show("Ngạt mũi?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //        //}
+            //        //if(test6 == false)
+            //        //{
+            //        //    DialogResult result4 = MessageBox.Show("Sốt cao?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //        //}
+            //        //DialogResult result8 = MessageBox.Show("Đau nhói vùng họng?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //        //DialogResult result9 = MessageBox.Show("Khan tiếng/ Tắt tiếng?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //        //DialogResult result10 = MessageBox.Show("Hôi hơi thở?", "Chi tiết triệu chứng", MessageBoxButtons.YesNo);
+            //    }
 
+            //}
         }  
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
@@ -109,12 +120,17 @@ namespace WF_Expert_System
         }
         private void button_clickReset_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < checkedListBox1.Items.Count; i++)
-            {
-                checkedListBox1.SetItemChecked(i, false);
-                contentchoiceListbox.Items.Clear();
-                listBox1.Items.Clear();
-            }
+            //for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            //{
+            //    checkedListBox1.SetItemChecked(i, false);
+            //    contentchoiceListbox.Items.Clear();
+            //    listBox1.Items.Clear();
+            //}
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
